261f07b79b822c3b07eb9365d43c076769b5971a
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-23 07:40:09 -0500

    implemented chat - breaks app

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,57 @@
 import React from 'react';
-import { View, Text} from 'react-native';
-
+import { View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      message: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+          image: null,
+          video: null,
+          sent: true,
+          received: true,
+          pending: true,
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
   render() {
     const { username, backgroundColor } = this.props.route.params;
 
     this.props.navigation.setOptions({ title: username });
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: backgroundColor}}>
-        <Text>Cool background color choice!</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backgroundColor }}>
+        <Text>helo</Text>
+        {/* <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        /> */}
       </View>
     )
   }
